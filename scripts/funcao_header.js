552567599e3880c075c5e55125dfd5f92736f215
HENRIQUE VIEIRA JUSTUS <henrique_justus@estudante.sesisenai.org.br>
2025-09-25 10:18:02 -0300

    teste

--- scripts/funcao_header.js
+++ scripts/funcao_header.js
@@ -6,3 +6,24 @@ document.getElementById('linkVoltar').addEventListener('click', function (event)
     event.preventDefault();
     history.back();
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.querySelector(".dropdown-btn");
+    const menu = document.querySelector(".dropdown-content");
+
+    btn.addEventListener("click", () => {
+        menu.classList.toggle("show");
+    });
+
+    document.addEventListener("click", (e) => {
+        if (!btn.contains(e.target) && !menu.contains(e.target)) {
+            menu.classList.remove("show");
+        }
+    });
+
+    menu.querySelectorAll("a").forEach(link => {
+        link.addEventListener("click", () => {
+            menu.classList.remove("show");
+        });
+    });
+});
